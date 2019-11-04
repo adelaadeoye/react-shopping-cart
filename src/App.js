960f72a15bb20e5a55d1b0590f09159d21d42f09
adeoye adela <adeoye.adela@gmail.com>
2019-11-04 23:50:57 +0100

    Cart persist using local Storage

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 
 //Context
-import {ProductContext} from "./contexts/ProductContext";
-import {CartContext} from "./contexts/CartContext";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { useLocalStorage } from "./components/useLocalStorage";
 
 function App() {
   const [products] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useLocalStorage("data",[]);
 
   const addItem = item => {
     // add the given item to the cart
+
     const newCartItem = {
       id: new Date(),
       title: item.title,
       price: item.price,
       image: item.image
-	};
+    };
 
     setCart([...cart, newCartItem]);
   };
 
-  const removeItem=productID=>{
-
-	  const newCart=cart.filter(item=>item.id !==productID);
-	  setCart(newCart);
-	console.log(productID)
-  }
-
+  const removeItem = productID => {
+    const newCart = cart.filter(item => item.id !== productID);
+    setCart(newCart);
+    console.log(productID);
+  };
   return (
-    <div className="App">
-      <ProductContext.Provider value={{ products, addItem}}>
-      <CartContext.Provider value={{removeItem,cart}}>
-        <Navigation cart={cart} />
+    <>
 
-        {/* Routes */}
-        <Route
-          exact path="/"
-         component= {Products}
-        />
+      <div className="App">
+        <ProductContext.Provider value={{ products, addItem }}>
+          <CartContext.Provider value={{ removeItem, cart }}>
+            <Navigation />
 
-        <Route path="/cart" component={ShoppingCart} />
-		</CartContext.Provider>
-      </ProductContext.Provider>
-    </div>
+            {/* Routes */}
+            <Route exact path="/" component={Products} />
+
+            <Route path="/cart" component={ShoppingCart} />
+          </CartContext.Provider>
+        </ProductContext.Provider>
+      </div>
+    </>
   );
 }
 
